8d1869f00a0a3377d018ed0ead228b6b3008da74
Sam Nolan <13807753+Hazelfire@users.noreply.github.com>
2022-12-09 04:50:17 +0000

    Implement basic complete QALY shop

--- src/App.tsx
+++ src/App.tsx
@@ -26,47 +26,184 @@ const card_pins: { [k: string]: string } = {
   'aa20a4f1-326a-4021-a4ad-eab79f422d20': "969528"
 }
 
+type PageName = "Pin" | "Selection" | "Allocation" | "Confirmation"
 function App() {
-  let [isValid, setIsValid] = React.useState(false)
+  let [page, setPage] = React.useState<PageName>("Pin")
+  let [chosenCharities, setChosenCharities] = React.useState<string[]>([])
+  let [allocations, setAllocations] = React.useState<Allocations>({});
+  let [isSending, setIsSending] = React.useState(true);
+  let [error, setError] = React.useState("");
 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        {isValid ? <SelectionPage /> : <PinPage onSuccess={() => setTimeout(() => setIsValid(true), 1547)} />}
-      </header>
-    </div>
-  );
+  if(page === "Pin"){
+    return (
+      <div className="App">
+        <PinPage onSuccess={() => setTimeout(() => setPage("Selection"), 1547)} />
+      </div>
+    )
+  }
+  else if(page === "Selection"){
+    return (
+      <div className="App">
+        <SelectionPage value={chosenCharities} onSelectedCharities={setChosenCharities} onSubmit={() => {
+          if(chosenCharities.length === 1){
+            setPage("Confirmation")
+            setAllocations({[chosenCharities[0]]: 100});
+            sendAllocation(allocations).then(() => setIsSending(false)).catch(err => {
+              setIsSending(false);
+              setError(err);
+            })
+          }
+          else {
+            setPage("Allocation")
+            setAllocations(Object.fromEntries(chosenCharities.map(c => [c, 100 / chosenCharities.length])))
+          }
+        }}/>
+      </div>
+    )
+  }
+  else if(page === "Allocation"){
+    return (
+      <div className="App">
+        <AllocationPage charities={chosenCharities} value={allocations} onAllocateCharities={setAllocations} onSubmit={() => {
+          setPage("Confirmation")
+          sendAllocation(allocations).then(() => setIsSending(false)).catch(err => {
+            setIsSending(false);
+            setError(err);
+          })
+        }}/>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className="App">
+        <ConfirmationPage error={error} isSending={isSending} allocations={allocations} />
+      </div>
+    )
+  }
+}
+let sendAllocation = (allocations: Allocations) => {
+  let cardId = new URLSearchParams(window.location.search).get("card_id");
+  let data = {
+    content: `You just got a submission of charity gift card ${cardId}!\n\n${Object.entries(allocations).map(([name, allocation]) => ` - ${name}: ${formatCurrency(allocation)}`).join("\n")}`
+  }
+  return fetch("https://discord.com/api/webhooks/1050620734885613639/ZhaTPchuKjWN8uy4fmkoTcTrIpfUQT6OPQOrwulcZ9CEwCwXkNUvVduk2qFYbL6nyHJJ", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)});
 }
 
 function PinPage(props: { onSuccess: () => void }) {
   let [codeField, setCodeField] = React.useState("")
+  let [fieldDisabled, setFieldDisabled] = React.useState(false);
   let [err, setError] = React.useState("")
   let card_id = new URLSearchParams(window.location.search).get("card_id");
 
+  let submit = () => {
+    if (card_pins[card_id ?? ""] === codeField) {
+      setFieldDisabled(true)
+      props.onSuccess()
+    } else {
+      setError("Incorrect pin")
+    }
+  }
   if (card_id && card_id in card_pins) {
-    return <form action="javascript:void(0);" onSubmit={() => {
-      if (card_pins[card_id ?? ""] === codeField) {
-        props.onSuccess()
-      } else {
-        setError("Incorrect pin")
-      }
-    }}>
-      <h1>Redeem your gift card</h1>
-      <p>Enter your pin</p>
-      <input onChange={(e) => {
-        setCodeField(e.target.value);
-        setError("")
-      }} />
-      <p>{err}</p>
-    </form>
+    return <header className="App-header">
+      <form action="javascript:void(0);" onSubmit={submit}>
+        <h1>Redeem your gift card</h1>
+        <p>Enter your pin</p>
+        <input onChange={(e) => {
+          setCodeField(e.target.value);
+          setError("")
+        }} disabled={fieldDisabled} />
+        <button onClick={submit}>Verify</button>
+        <p>{err}</p>
+      </form>
+    </header>
   } else {
     return <p>You should not access this page, as you do not have a valid card</p>;
   }
 }
 
-function SelectionPage(props: {}) {
-  return <div>Chose your charity!</div>
+type Charity = {
+  name: string
 }
 
+let charityOptions : Charity[] = [
+  { name: "New Incentives" },
+  { name: "Top Charities Fund" },
+  { name: "The Humane League" },
+  { name: "Strongminds" },
+  { name: "Helen Keler International" },
+  { name: "Sightsavers" },
+  { name: "Family Empowerment Media" },
+  { name: "Nuclear Threat Initiative" },
+  { name: "Clean Air Task Force" }
+]
+
+function SelectionPage(props: {value: string[], onSelectedCharities: (x: string[]) => void, onSubmit: () => void}) {
+
+  return <div><header className="App-header"><h1>Choose your charities</h1>
+    <p>You will be able to choose how much you would like to allocate to each charity on the next page.</p>
+    <select value={props.value} multiple={true} onChange={e => props.onSelectedCharities(Array.from(e.target.selectedOptions).map(({ value }) => value))}>
+      {charityOptions.map(charity => <option key={charity.name} value={charity.name}>{charity.name}</option>)}
+    </select>
+    <button onClick={() => props.onSubmit()}>{props.value.length > 1? "Allocate Donation": "Donate"}</button>
+    </header>
+  </div>
+}
+type Allocations = {[key: string]: number}
+
+function AllocationPage(props: {value: Allocations, onAllocateCharities: (x: Allocations) => void, charities: string[], onSubmit: () => void}){
+
+  let setAllocation: (k: string) => React.ChangeEventHandler<HTMLInputElement> =(name) =>  (e) => {
+    let newValue = parseFloat(e.target.value);
+    let newSum = 100 - newValue;
+    let oldSum = 100 - props.value[name];
+    let newAllocations = Object.entries(props.value).map(([key, value]) => {
+      if(key === name){
+        return [key, newValue];
+      }
+      else {
+        return [key, oldSum === 0 ? newSum / (props.charities.length - 1) : value / oldSum * newSum];
+      }
+    })
+    props.onAllocateCharities(Object.fromEntries(newAllocations));
+  }
+  return <div><header className="App-header"><h1>Allocate your donation</h1>
+    <p>Choose how much you would like to donate to each charity</p>
+    {Object.entries(props.value).map(([name, alloc]) => <div><strong>{name}</strong><input type="range" min={0} max={100} value={alloc} onChange={setAllocation(name)} /><span>{formatCurrency(alloc)}</span></div>)}
+      <button onClick={() => props.onSubmit()}>Donate</button>
+    </header>
+  </div>
+}
+
+function ConfirmationPage(props: {error: string; isSending: boolean; allocations: Allocations}) {
+  let cardId = new URLSearchParams(window.location.search).get("card_id");
+  React.useEffect(() => {
+  }, [])
+
+  if(props.isSending){
+    return <div><header className="App-header"><h1>Recording your donation...</h1></header></div>;
+  }
+  else {
+    if(props.error !== ""){
+      return <div><header className="App-header"><h1>There was an error recording your donation</h1>
+        <p>{props.error}</p></header>
+      </div>
+    }
+    else {
+      return <div><header className="App-header"><h1>Thank you for you donation!</h1>
+        <p>Your donation will be sent to the following charities:</p>
+        <ul>
+        {Object.entries(props.allocations).map(([name, alloc]) => <li key={name}><strong>{name}</strong>: {formatCurrency(alloc)}</li>)}
+        </ul></header>
+      </div>
+    }
+  }
+}
+
+let formatCurrency = (x: number) => new Intl.NumberFormat('en-US', {style: 'currency', maximumFractionDigits: 2, currency: "AUD"}).format(x)
 export default App;
